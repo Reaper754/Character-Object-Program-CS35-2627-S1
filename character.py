51d8bf2188b09cb114f8366bb58481778ee35e5c
Reaper754 <MasonT019@stemcollegiate.ca>
2026-09-14 09:53:22 -0600

    Finished

--- character.py
+++ character.py
@@ -4,3 +4,72 @@ class Character:
         self.weapon = weapon
         self.name = name
         self.ammo = ammo
+        self.rank = "Private"
+
+    def shoot(self):
+        if self.health == 0:
+            print(f"{self.rank} {self.name} is downed and cannot shoot.")
+        else:
+            if self.ammo == 0:
+                print (f"Click. {self.rank} {self.name} needs to reload.")
+            elif self.weapon == "M249 SAW":
+                self.ammo -= 10
+                print(f"Brrrr! {self.rank} {self.name} fired ten rounds.")
+            else:
+                self.ammo -= 1
+                print(f"Bang! {self.rank} {self.name} fired a round.")
+
+    def damage(self):
+        if self.health == 0:
+            print(f"{self.rank} {self.name} is already downed.")
+        else:
+            amount = int(input(f"How much damage should {self.rank} {self.name} take?"))
+            self.health -= amount
+            print(f"{self.rank} {self.name} took {amount} points of damage.")
+            if self.health == 0:
+                print(f"{self.rank} {self.name} is now downed")
+            elif self.health < 0:
+                self.health = 0
+                print(f"{self.rank} {self.name} is now downed")
+
+    def reload(self):
+        if self.health == 0:
+            print(f"{self.rank} {self.name} is downed and cannot reload.")
+        else:
+            if self.ammo != 0:
+                if self.weapon == "M249 SAW":
+                    self.ammo = 201
+                    print(f"{self.rank} {self.name} reloaded to 201")
+                else:
+                    self.ammo = 31
+                    print(f"{self.rank} {self.name} reloaded to 31")
+            else:
+                if self.weapon == "M249 SAW":
+                    self.ammo = 200
+                    print(f"{self.rank} {self.name} reloaded to 200")
+                else:
+                    self.ammo = 30
+                    print(f"{self.rank} {self.name} reloaded to 30")
+
+    def heal(self):
+        if self.name == "Johnson":
+            if self.health == 150:
+                print(f"{self.rank} {self.name} are already at max health.")
+            else:
+                self.health += 50
+                print(f"{self.rank} {self.name} has healed 50 health points.")
+                self.stats()
+                if self.health > 150:
+                    self.health = 150
+        else:
+            if self.health == 100:
+                print(f"{self.rank} {self.name} are already at max health.")
+            else:
+                self.health += 50
+                print(f"{self.rank} {self.name} has healed 50 health points.")
+                self.stats()
+                if self.health > 100:
+                    self.health = 100
+
+    def stats(self):
+        print(f"health: {self.health}, Weapon: {self.weapon}, Name: {self.name}, Ammo: {self.ammo}, Rank: {self.rank}")
--- main.py
+++ main.py
@@ -1,2 +1,65 @@
 from character import Character
 
+assault = Character(100, "M4A1 Carbine", "Green", 30)
+heavy = Character(150, "M249 SAW", "Johnson", 200)
+
+select = """Choose your class:
+1 for assault | 2 for heavy | 3 for exit\n"""
+
+message = """Enter an input: 
+| Shoot | Damage | Reload | Heal | Stats |\n"""
+
+commands = [
+    "shoot",
+    "damage",
+    "reload",
+    "heal",
+    "stats",
+]
+
+actions = [
+    assault.shoot,
+    assault.damage,
+    assault.reload,
+    assault.heal,
+    assault.stats,
+]
+
+actions2 = [
+    heavy.shoot,
+    heavy.damage,
+    heavy.reload,
+    heavy.heal,
+    heavy.stats,
+]
+
+while True:
+    try:
+        soldier = int(input(select))
+        if soldier == 1:
+            user_input = input(message).strip().lower()
+            for command, action in zip(commands, actions):
+                if user_input == command:
+                    action()
+                    break
+                else:
+                    if user_input == "exit":
+                        break
+                    print("Invalid command.")
+        elif soldier == 2:
+            user_input = input(message).strip().lower()
+            for command, action2 in zip(commands, actions):
+                if user_input == command:
+                    action2()
+                    break
+                else:
+                    if user_input == "exit":
+                        break
+                    print("Invalid command.")
+        else:
+            if soldier == 3:
+                break
+            print("Invalid command.")
+    except ValueError:
+        print("Invalid input")
+
